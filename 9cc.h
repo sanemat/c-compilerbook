d653b9f142268d0511facd1d34c987a035c3bb92
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-04-08 00:08:30 +0700

    refactor(parse): extract to parse.c

--- 9cc.h
+++ 9cc.h
@@ -35,3 +35,28 @@ Token *tokenize();
 
 extern char *user_input;
 extern Token *token;
+
+// parse.c
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  int val;       // Used if kind == ND_NUM
+};
+
+Node *expr();
