00489c3af109361c675895c2bd87e1f33143bd66
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-04-08 00:01:59 +0700

    refactor(tokenize): split to tokenize

---
+++ 9cc.h
@@ -0,0 +1,37 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// tokenize.c
+
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // Token length
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+Token *tokenize();
+
+extern char *user_input;
+extern Token *token;
